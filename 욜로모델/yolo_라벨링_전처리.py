b3953fc37d1b75c218b464b1037fd83d8be33a46
gukjin-1 <droplet__@likelion.org>
2022-06-08 19:30:07 +0900

    욜로라벨링전처리

---
+++ 욜로모델/yolo_라벨링_전처리.py
@@ -0,0 +1,13 @@
+# txt 내용 바꾸기
+def replace_in_file(file_path, old_str, new_str):
+    # 파일 읽어들이기
+    fr = open(file_path, 'r')
+    lines = fr.readlines()
+    lines = lines[1]
+    fr.close()
+    
+    # old_str -> new_str 치환
+    fw = open(file_path, 'w')
+    for line in lines:
+        fw.write(line.replace(old_str, new_str))
+    fw.close()
